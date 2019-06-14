a081b9a1492c1e537fcd58b3f6456542597f9c80
jhpeng <alanpeng1995@gmail.com>
2019-06-14 21:30:11 +0800

    diagonal updat(need to speed up and debug)

--- cuda/main.cu
+++ cuda/main.cu
@@ -137,11 +137,35 @@ void device_destroy_placeholder(){
 ** ------------------ SSE algorithm ------------------- **
 ** ---------------------------------------------------- */
 
+__device__ void device_propagate_state(int* d_sigmap, int* d_bond2index, int sp, int nsite, int id){
+    int i_bond = sp/6;
+    int type   = sp%6;
+
+    if(sp==-1 || type==0 || type==2) return;
+    else if(type==1 || type==4){
+        d_sigmap[id*nsite+d_bond2index[i_bond*4+0]] *= -1;
+        d_sigmap[id*nsite+d_bond2index[i_bond*4+1]] *= -1;
+    }
+    else if(type==3){
+        d_sigmap[id*nsite+d_bond2index[i_bond*4+2]] *= -1;
+        d_sigmap[id*nsite+d_bond2index[i_bond*4+3]] *= -1;
+    }
+    else if(type==5){
+        d_sigmap[id*nsite+d_bond2index[i_bond*4+0]] *= -1;
+        d_sigmap[id*nsite+d_bond2index[i_bond*4+1]] *= -1;
+        d_sigmap[id*nsite+d_bond2index[i_bond*4+2]] *= -1;
+        d_sigmap[id*nsite+d_bond2index[i_bond*4+3]] *= -1;
+    }
+    return;
+}
+
 __global__ void device_diagonal_update(int nstream, int nsite, int nj, int nq, double beta, int is, int* d_length, int* d_noo, int* d_sequence, int* d_sigma0, int* d_sigmap, int* d_bond2index, double* d_bondst, curandState* d_curng){
     int id = blockIdx.x*blockDim.x+threadIdx.x;
     if(id<nstream){
-        int p_max = (int)(*d_length*(id+1)/nstream);
-        int p_min = (int)(*d_length*id/nstream);
+        int length = *d_length;
+        int noo = *d_noo;
+        int p_max = (int)(length*(id+1)/nstream);
+        int p_min = (int)(length*id/nstream);
         int i,p,i_bond,s1,s2,s3,s4;
         double dis;
 
@@ -149,17 +173,90 @@ __global__ void device_diagonal_update(int nstream, int nsite, int nj, int nq, d
 
         if(id==is){
             for(p=p_min;p<p_max;++p){
-                i_bond = (int)curand_uniform_double(&d_curng[id]);
-                s1 = d_sigmap[id*nsite+d_bond2index[i_bond*4+0]];
-                s2 = d_sigmap[id*nsite+d_bond2index[i_bond*4+1]];
-                s3 = d_sigmap[id*nsite+d_bond2index[i_bond*4+2]];
-                s4 = d_sigmap[id*nsite+d_bond2index[i_bond*4+3]];
-                if(i_bond<nj && s1!=s2){
-                    
+                if(d_sequence[p]==-1){
+                    i_bond = (int)((nj+nq)*curand_uniform_double(&d_curng[id]));
+                    s1 = d_sigmap[id*nsite+d_bond2index[i_bond*4+0]];
+                    s2 = d_sigmap[id*nsite+d_bond2index[i_bond*4+1]];
+                    s3 = d_sigmap[id*nsite+d_bond2index[i_bond*4+2]];
+                    s4 = d_sigmap[id*nsite+d_bond2index[i_bond*4+3]];
+                    if(i_bond<nj && s1!=s2){
+                        dis = curand_uniform_double(&d_curng[id]);
+                        if(dis*2*(length-noo)<beta*d_bondst[i_bond]*(nj+nq)){
+                            d_sequence[p] = i_bond*6;
+                            noo++;
+                        }
+                    }
+                    else if(s1!=s2 && s3!=s4){
+                        dis = curand_uniform_double(&d_curng[id]);
+                        if(dis*4*(length-noo)<beta*d_bondst[i_bond]*(nj+nq)){
+                            d_sequence[p] = i_bond*6+2;
+                            noo++;
+                        }
+                    }
                 }
+                else if(d_sequence[p]%6==0){
+                    i_bond = d_sequence[p]/6;
+                    dis = curand_uniform_double(&d_curng[id]);
+                    if(beta*(nj+nq)*d_bondst[i_bond]*dis<2*(length-noo+1)){
+                        d_sequence[p] = -1;
+                        noo--;
+                    }
+                }
+                else if(d_sequence[p]%6==2){
+                    i_bond = d_sequence[p]/6;
+                    dis = curand_uniform_double(&d_curng[id]);
+                    if(beta*(nj+nq)*d_bondst[i_bond]*dis<4*(length-noo+1)){
+                        d_sequence[p] = -1;
+                        noo--;
+                    }
+                }
+                else device_propagate_state(d_sigmap,d_bond2index,d_sequence[p],nsite,id);
             }
+            *d_noo = noo;
+            printf("%d\n",noo);
         }
         else{
+            for(p=p_min;p<p_max;++p){
+                if(d_sequence[p]%6==0){
+                    i_bond = (int)((nj+nq)*curand_uniform_double(&d_curng[id]));
+                    s1 = d_sigmap[id*nsite+d_bond2index[i_bond*4+0]];
+                    s2 = d_sigmap[id*nsite+d_bond2index[i_bond*4+1]];
+                    s3 = d_sigmap[id*nsite+d_bond2index[i_bond*4+2]];
+                    s4 = d_sigmap[id*nsite+d_bond2index[i_bond*4+3]];
+                    if(i_bond<nj && s1!=s2){
+                        dis = curand_uniform_double(&d_curng[id]);
+                        if(dis*d_bondst[d_sequence[p]/6]<d_bondst[i_bond]){
+                            d_sequence[p] = i_bond*6;
+                        }
+                    }
+                    else if(s1!=s2 && s3!=s4){
+                        dis = curand_uniform_double(&d_curng[id]);
+                        if(dis*d_bondst[d_sequence[p]/6]*2<d_bondst[i_bond]){
+                            d_sequence[p] = i_bond*6+2;
+                        }
+                    }
+                }
+                else if(d_sequence[p]%6==2){
+                    i_bond = (int)((nj+nq)*curand_uniform_double(&d_curng[id]));
+                    s1 = d_sigmap[id*nsite+d_bond2index[i_bond*4+0]];
+                    s2 = d_sigmap[id*nsite+d_bond2index[i_bond*4+1]];
+                    s3 = d_sigmap[id*nsite+d_bond2index[i_bond*4+2]];
+                    s4 = d_sigmap[id*nsite+d_bond2index[i_bond*4+3]];
+                    if(i_bond<nj && s1!=s2){
+                        dis = curand_uniform_double(&d_curng[id]);
+                        if(dis*d_bondst[d_sequence[p]/6]<d_bondst[i_bond]*2){
+                            d_sequence[p] = i_bond*6;
+                        }
+                    }
+                    else if(s1!=s2 && s3!=s4){
+                        dis = curand_uniform_double(&d_curng[id]);
+                        if(dis*d_bondst[d_sequence[p]/6]<d_bondst[i_bond]){
+                            d_sequence[p] = i_bond*6+2;
+                        }
+                    }
+                }
+                else device_propagate_state(d_sigmap,d_bond2index,d_sequence[p],nsite,id);
+            }
         }
     }
 }
@@ -222,7 +319,9 @@ void device_set_lattice_jq_isotropy_2d(int nstream, int length, int nx, int ny, 
 
 int main()
 {
-    int nstream=2048;
+    int nstream=1024;
+    int nblock =128;
+    int nthread=(int)(nstream/nblock);
     int length=1000000;
     int nx=256;
     int ny=256;
@@ -233,6 +332,11 @@ int main()
     gsl_rng_set(rng,seed);
 
     device_set_lattice_jq_isotropy_2d(nstream,length,nx,ny,jbond,beta,rng);
+    for(int i=0;i<1000;++i){
+        int is = (int)(gsl_rng_uniform_pos(rng)*Nstream);
+        device_diagonal_update<<<nblock,nthread>>>(nstream,Nsite,Nj,Nq,Beta,is,d_Length,d_Noo,d_Sequence,d_Sigma0,d_Sigmap,d_Bond2index,d_Bondst,d_Curng);
+        cudaDeviceSynchronize();
+    }
 
     gsl_rng_free(rng);
     device_destroy_placeholder();
